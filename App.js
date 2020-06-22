5bc7af06f4eccd4ef5969e90dd78403b759558ce
Juan Huttemann <juanfhuttemann@gmail.com>
2020-06-21 22:13:59 -0400

    add basic navigation

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import * as React from 'react';
 import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => {
+function HomeScreen() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
     </View>
   );
-};
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
