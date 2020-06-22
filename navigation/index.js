fc37933c74fbb810cd35baa8115420969b24d949
Juan Huttemann <juanfhuttemann@gmail.com>
2020-06-21 22:43:06 -0400

    fix navigation

--- navigation/index.js
+++ navigation/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HostsStack from './stacks/HostsStack';
+import HostsStack from '../stacks/HostsStack';
 
 const Tab = createBottomTabNavigator();
 
